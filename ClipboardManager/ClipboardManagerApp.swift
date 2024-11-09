f8e1bc28ce7dcb2fd9d54a4798a8ee579c0aefc9
Soumik Sarkhel <soumik.sarkhel@p360.com>
2024-11-09 03:44:36 -0500

    Initial Commit

---
+++ ClipboardManager/ClipboardManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardManagerApp.swift
+//  ClipboardManager
+//
+//  Created by Soumik Sarkhel on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
